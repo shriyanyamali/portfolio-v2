1039230bb30ab53bf4f1a6a1945c10c8913573ff
ShriyanYamali <srujanshriyan@gmail.com>
2024-08-25 01:19:14 -0400

    updated website links on demo-hover-effects.jsx

--- src/components/demo/demo-hover-effect.jsx
+++ src/components/demo/demo-hover-effect.jsx
@@ -12,36 +12,36 @@ export const projects = [
     title: "Stanford Law School",
     description:
       "Stanford Law School is the highest rated law school in the United States, known for its innovative approach to legal education and its close ties to Silicon Valley.",
-    link: "https://stripe.com",
+    link: "https://law.stanford.edu/",
   },
   {
     title: "University of Pennsylvania Carey Law School",
     description:
       "Penn Carey Law is the 4th highest rated law school in the United States, with a prestigious, cutting-edge legal education system.",
-    link: "https://netflix.com",
+    link: "https://www.law.upenn.edu/",
   },
   {
     title: "Dartmouth College",
     description:
       "Dartmouth is an Ivy League college known for its strong liberal arts education and scenic landscapes.",
-    link: "https://google.com",
+    link: "https://home.dartmouth.edu/",
   },
   {
     title: "Wilmington University School of Law",
     description:
       "Wilmington University School of Law is one of the few law schools in the state of Delaware. It provides a strong legal education and is a top university in Delaware.",
-    link: "https://meta.com",
+    link: "https://www.wilmu.edu/",
   },
   {
-    title: "American Civil Liberties Union",
+    title: "American Civil Liberties Union of Delaware",
     description:
-      "The American Civil Liberties Union is a non-profit organization committed to protecting the constitutional rights and freedoms of individuals through advocacy and litigation.",
-    link: "https://amazon.com",
+      "The American Civil Liberties Union of Delaware is a non-profit organization committed to protecting the constitutional rights and freedoms of individuals through advocacy and litigation.",
+    link: "https://www.aclu-de.org/",
   },
   {
     title: "Delaware Museum of Nature & Science",
     description:
       "The Delaware Museum of Nature & Science engages visitors with interactive exhibits and programs focused on the wonders of nature and science. It boasts some of the world's largest collections of birds, eggs, and other organisms.",
-    link: "https://microsoft.com",
+    link: "https://delmns.org/",
   },
 ];
