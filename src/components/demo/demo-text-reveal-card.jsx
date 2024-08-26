1cbceda4b1873eb451323dcd4dc34dede312f2d4
ShriyanYamali <srujanshriyan@gmail.com>
2024-08-26 09:24:49 -0400

    changed text reveal card text

--- src/components/demo/demo-text-reveal-card.jsx
+++ src/components/demo/demo-text-reveal-card.jsx
@@ -9,7 +9,7 @@ import {
 export function TextRevealCardPreview() {
   return (
     <div className="flex items-center justify-center rounded-2xl w-full pb-8">
-      <TextRevealCard text="You Know What You Want" revealText="I Know How To Do It">
+      <TextRevealCard text="You Know The Business" revealText="And I Know The Chemistry">
       </TextRevealCard>
     </div>
   );
