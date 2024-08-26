6bbf461bf2d657dbe5e139c65c084e6e0178f53a
ShriyanYamali <srujanshriyan@gmail.com>
2024-08-26 17:00:43 -0400

    changed ' to &rsquo;

--- src/components/demo/demo-glare-card.jsx
+++ src/components/demo/demo-glare-card.jsx
@@ -12,7 +12,7 @@ export function GlareCardDemo() {
         >
            <path d="M320-242q10 1 19.5 1.5t20.5.5 20.5-.5T400-242v82h400v-400h-82q1-10 1.5-19.5t.5-20.5-.5-20.5T718-640h82q33 0 56.5 23.5T880-560v400q0 33-23.5 56.5T800-80H400q-33 0-56.5-23.5T320-160zm40-78q-117 0-198.5-81.5T80-600t81.5-198.5T360-880t198.5 81.5T640-600t-81.5 198.5T360-320m0-80q83 0 141.5-58.5T560-600t-58.5-141.5T360-800t-141.5 58.5T160-600t58.5 141.5T360-400m0-200"/>
         </svg>
-      <p className="text-white font-bold text-xl mt-4">Let's Chat</p>
+      <p className="text-white font-bold text-xl mt-4">Let&rsquo;s Chat</p>
     </GlareCard>)
   );
 }
