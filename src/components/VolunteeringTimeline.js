db6f7eab7075a8297823c29839fa97c0f1828b28
ShriyanYamali <srujanshriyan@gmail.com>
2024-12-21 18:16:07 -0500

    reordered experiences and updated descriptions

--- src/components/VolunteeringTimeline.js
+++ src/components/VolunteeringTimeline.js
@@ -5,7 +5,7 @@ const VolunteeringTimeline = () => {
   return (
     <div className="flex flex-col items-center justify-center">
       <ol className="relative border-s border-gray-200 dark:border-gray-700 max-w-[50rem] lg:max-w-full">
-      <motion.div
+        <motion.div
           initial={{ y: 50 }}
           whileInView={{ y: 0 }}
           transition={{ duration: 0.5, type: "spring" }}
@@ -19,15 +19,15 @@ const VolunteeringTimeline = () => {
                 fill="currentColor"
                 viewBox="0 0 512 512"
               >
-               <path d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z"/>
-            </svg>
+                <path d="M184 48l144 0c4.4 0 8 3.6 8 8l0 40L176 96l0-40c0-4.4 3.6-8 8-8zm-56 8l0 40L64 96C28.7 96 0 124.7 0 160l0 96 192 0 128 0 192 0 0-96c0-35.3-28.7-64-64-64l-64 0 0-40c0-30.9-25.1-56-56-56L184 0c-30.9 0-56 25.1-56 56zM512 288l-192 0 0 32c0 17.7-14.3 32-32 32l-64 0c-17.7 0-32-14.3-32-32l0-32L0 288 0 416c0 35.3 28.7 64 64 64l384 0c35.3 0 64-28.7 64-64l0-128z" />
+              </svg>
             </span>
             <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white mg:text-base sm:!text-sm">
-              Archivist Volunteer&nbsp;
+              Treasurer &nbsp;
               <div className="text-primary dark:text-primaryDark sm:ml-3">
-                @American Civil Liberties Union of Delaware{" "}
+                @Delaware Museum of Nature & Science{" "}
               </div>
-                <span className="bg-blue-800 text-blue-100 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-blue-300 dark:text-blue-900 ms-3 lg:hidden">
+              <span className="bg-blue-800 text-blue-100 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-blue-300 dark:text-blue-900 ms-3 lg:hidden">
                 On-Site
               </span>
             </h3>
@@ -35,7 +35,8 @@ const VolunteeringTimeline = () => {
               May 2024 — Present | Wilmington, Delaware
             </time>
             <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-300">
-              Adding to the ACLU of Delaware Archives by organizing, cataloging, scanning, and digitizing historical and legal documents.
+              Coordinating logistics and finances for setting up educational
+              science cafe events for children; 40+ hours of volunteering
             </p>
           </li>
         </motion.div>
@@ -47,22 +48,58 @@ const VolunteeringTimeline = () => {
         >
           <li className="mb-10 ms-6">
             <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -start-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
-            <svg
+              <svg
                 className="w-2.5 h-2.5 text-blue-800 dark:text-blue-300"
                 aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="currentColor"
                 viewBox="0 0 512 512"
               >
-               <path d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z"/>
-            </svg>
+                <path d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z" />
+              </svg>
+            </span>
+            <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white mg:text-base sm:!text-sm">
+              Archivist Volunteer&nbsp;
+              <div className="text-primary dark:text-primaryDark sm:ml-3">
+                @American Civil Liberties Union of Delaware{" "}
+              </div>
+              <span className="bg-blue-800 text-blue-100 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-blue-300 dark:text-blue-900 ms-3 lg:hidden">
+                On-Site
+              </span>
+            </h3>
+            <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+              May 2024 — Present | Wilmington, Delaware
+            </time>
+            <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-300">
+              Organizing and cataloging historical and legal documents; 50+
+              hours of volunteering.
+            </p>
+          </li>
+        </motion.div>
+
+        <motion.div
+          initial={{ y: 50 }}
+          whileInView={{ y: 0 }}
+          transition={{ duration: 0.5, type: "spring" }}
+        >
+          <li className="mb-10 ms-6">
+            <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -start-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+              <svg
+                className="w-2.5 h-2.5 text-blue-800 dark:text-blue-300"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 512 512"
+              >
+                <path d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z" />
+              </svg>
             </span>
             <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white mg:text-base sm:!text-sm">
               Lead Volunteer &nbsp;
               <div className="text-primary dark:text-primaryDark sm:ml-3">
                 @Shirdi Sai of Delaware{" "}
               </div>
-                <span className="bg-blue-800 text-blue-100 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-blue-300 dark:text-blue-900 ms-3 lg:hidden">
+              <span className="bg-blue-800 text-blue-100 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-blue-300 dark:text-blue-900 ms-3 lg:hidden">
                 On-Site
               </span>
             </h3>
@@ -70,14 +107,12 @@ const VolunteeringTimeline = () => {
               June 2018 — Aug 2023 | Newark, Delaware
             </time>
             <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-300">
-              Worked to coordinate events for over 500 people | Accumulated more
-              than 200 volunteer hours.
+              Worked to coordinate religious gatherings for over 500 people;
+              200+ hours of volunteering.
             </p>
           </li>
         </motion.div>
-        </ol>
 
-        <ol className="relative ">
         <motion.div
           initial={{ y: 50 }}
           whileInView={{ y: 0 }}
@@ -85,22 +120,22 @@ const VolunteeringTimeline = () => {
         >
           <li className="mb-10 ms-6">
             <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -start-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
-            <svg
+              <svg
                 className="w-2.5 h-2.5 text-blue-800 dark:text-blue-300"
                 aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="currentColor"
                 viewBox="0 0 512 512"
               >
-               <path d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z"/>
-            </svg>
+                <path d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z" />
+              </svg>
             </span>
             <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white mg:text-base sm:!text-sm">
               Volunteer &nbsp;
               <div className="text-primary dark:text-primaryDark sm:ml-3">
                 @Food Bank of Delaware{" "}
               </div>
-                <span className="bg-blue-800 text-blue-100 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-blue-300 dark:text-blue-900 ms-3 lg:hidden">
+              <span className="bg-blue-800 text-blue-100 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-blue-300 dark:text-blue-900 ms-3 lg:hidden">
                 On-Site
               </span>
             </h3>
@@ -108,12 +143,11 @@ const VolunteeringTimeline = () => {
               Jan 2019 — Oct 2022 | Newark, Delaware
             </time>
             <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-300">
-              Helped package and distribute food throughout the year | Gained
-              more than 60 volunteer hours.
+              Helped package and distribute food; 60+ hours of volunteering.
             </p>
           </li>
         </motion.div>
-        </ol>
+      </ol>
     </div>
   );
 };
