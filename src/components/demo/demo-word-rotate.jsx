a0df88398479460afd959688570c43b560683e33
ShriyanYamali <srujanshriyan@gmail.com>
2025-03-13 16:50:39 -0400

    updated words in word rotate demo

--- src/components/demo/demo-word-rotate.jsx
+++ src/components/demo/demo-word-rotate.jsx
@@ -4,7 +4,7 @@ const WordRotateDemo = () => {
   return (
     <WordRotate
       className="text-4xl xtl:text-3xl lg:!text-4xl font-bold text-black dark:text-white"
-      words={["dreamer.", "Delawarean.", "believer.", "innovator.", "leader.", "student.", "programmer.", "writer.",
+      words={["student.", "programmer.", "writer.", "leader.", "Delawarean.", "innovator.",  
 ]}
     />
   );
